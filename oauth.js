5c0aecaf86c9c2f5f4470ffbb9fddea11a3f663c
mcmorco <88703483+mcmorco@users.noreply.github.com>
2022-02-25 21:50:57 +0800

    Working "sign-in"
    Signing in refers to authenticating the account

--- oauth.js
+++ oauth.js
@@ -1,10 +1,17 @@
 
-var el = document.querySelector('signin');
+/**var el = document.querySelector('signin');
 if(el){
     el.addEventListener('click', function() {
         chrome.identity.getAuthToken({interactive: true}, function(token) {
         console.log(token);
         });
     });
-}
+}**/
 
+//window.onload = function() {
+    document.querySelector('button').addEventListener('click', function() {
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
+      });
+    });
+//  };
