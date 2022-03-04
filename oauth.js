333fd7a197c4a9ccfe5f47babf0c924474d44306
mcmorco <88703483+mcmorco@users.noreply.github.com>
2022-03-04 11:53:56 +0800

    Fixed permissions bug and Time bug
    Added a modified version of Google's template code for removeCachedAuthToken https://developer.chrome.com/docs/apps/app_identity/
    Changed the time of the endDate for whole days to 00:00 instead of 23:59

--- oauth.js
+++ oauth.js
@@ -1,12 +1,29 @@
 main();
 
 function main() {
-  chrome.identity.getAuthToken({interactive: true}, function(token) {  
+  chrome.identity.getAuthToken({interactive: true}, function(token) { 
+    console.log(token); 
     var x = new XMLHttpRequest();
-    x.addEventListener("load", buildUpcomingEvents);
+    var retry = true;
+    //code modified from Google Developers for checking authentication
+    x.onload = function () {
+      if (this.status === 401 && retry) {
+        retry = false;
+        chrome.identity.removeCachedAuthToken(
+            { 'token': token },
+            main);
+        return;
+      }
+    }
+    x.addEventListener("load", buildUpcomingEvents,x);
     x.open('GET', 'https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=' + token, true);
     x.send();
-  });  
+  });
+  var eventsElement = document.getElementById("calendar_events");
+  eventsElement.appendChild(document.createElement("tr"));
+  addUpcomingEvent('th', "Event Name", eventsElement, 'text-align: left; padding-right:1rem');
+  addUpcomingEvent('th', "Start", eventsElement, 'text-align: left; padding-right:1rem');
+  addUpcomingEvent('th', "End", eventsElement, 'text-align: left');  
   buildCalendarDates();
 }
 
@@ -40,45 +57,46 @@ function getOffSet(){
   return sign + hours + ":" + mins;
 }
 
-function buildUpcomingEvents() {
+function buildUpcomingEvents(x) {
   const data = JSON.parse(this.responseText);
   var eventsElement = document.getElementById("calendar_events");
-  eventsElement.appendChild(document.createElement("tr"));
-  addUpcomingEvent('th', "Event Name", eventsElement, 'text-align: left; padding-right:1rem');
-  addUpcomingEvent('th', "Start", eventsElement, 'text-align: left; padding-right:1rem');
-  addUpcomingEvent('th', "End", eventsElement, 'text-align: left');
-  const listOfEvents = []
   
-  for(let i = 0; i < data['items'].length; i++){
-    if (data['items'][i]['start']['dateTime'] == null) {
-      var TZOffset = getOffSet();
-      listOfEvents.push({nameOfEvent:data['items'][i]['summary'], startDate:data['items'][i]['start']['date'].concat('','T00:00:00', TZOffset), endDate: data['items'][i]['end']['date'].concat('','T23:59:59',TZOffset)})
-    }else{
-      listOfEvents.push({nameOfEvent:data['items'][i]['summary'], startDate:data['items'][i]['start']['dateTime'], endDate: data['items'][i]['end']['dateTime']})
+  const listOfEvents = []
+  console.log(data['items']);
+  if(typeof data['items'] !== 'undefined'){
+    for(let i = 0; i < data['items'].length; i++){
+      if (data['items'][i]['start']['dateTime'] == null) {
+        var TZOffset = getOffSet();
+        listOfEvents.push({nameOfEvent:data['items'][i]['summary'], startDate:data['items'][i]['start']['date'].concat('','T00:00:00', TZOffset), endDate: data['items'][i]['end']['date'].concat('','T00:00:00',TZOffset)})
+      }else{
+        listOfEvents.push({nameOfEvent:data['items'][i]['summary'], startDate:data['items'][i]['start']['dateTime'], endDate: data['items'][i]['end']['dateTime']})
+      }
+    }
+  
+    listOfEvents.sort(objectCompare);
+    console.log(listOfEvents);
+
+    for(let i = 0; i < data['items'].length; i++){
+      var dateNow = new Date();
+      var eventDate = new Date(listOfEvents[i].endDate);
+      var diff = eventDate - dateNow;
+      console.log(dateNow.toISOString());
+      console.log(eventDate);
+      console.log(dateNow);
+      console.log(diff);
+      if(diff > 0){
+        console.log(listOfEvents[i].nameOfEvent);
+        eventsElement.appendChild(document.createElement("tr"));
+        addUpcomingEvent('td', listOfEvents[i].nameOfEvent, eventsElement,'padding-right:1rem');
+        addUpcomingEvent('td', listOfEvents[i].startDate, eventsElement,'padding-right:1rem');
+        addUpcomingEvent('td', listOfEvents[i].endDate, eventsElement);
+
+      }
+      
+
     }
   }
-  listOfEvents.sort(objectCompare);
-  console.log(listOfEvents);
 
-  for(let i = 0; i < data['items'].length; i++){
-    var dateNow = new Date();
-    var eventDate = new Date(listOfEvents[i].endDate);
-    var diff = eventDate - dateNow;
-    console.log(dateNow.toISOString());
-    console.log(eventDate);
-    console.log(dateNow);
-    console.log(diff);
-    if(diff > 0){
-      console.log(listOfEvents[i].nameOfEvent);
-      eventsElement.appendChild(document.createElement("tr"));
-      addUpcomingEvent('td', listOfEvents[i].nameOfEvent, eventsElement,'padding-right:1rem');
-      addUpcomingEvent('td', listOfEvents[i].startDate, eventsElement,'padding-right:1rem');
-      addUpcomingEvent('td', listOfEvents[i].endDate, eventsElement);
-
-    }
-    
-
-  }
 }
 
 function addUpcomingEvent(tag, text, eventsElement, style = null) {
